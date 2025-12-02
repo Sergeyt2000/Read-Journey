419705df68838086fc24bb4407e1b2e956168d68
Sergey Tkachenko <sergeyt2000@gmail.com>
2025-12-03 00:13:53 +0200

    fix styles Login/Reg

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,50 +6,91 @@ import { useDispatch } from "react-redux";
 import { login } from "../../redux/auth/operations.js";
 import AuthHead from "../AuthHead/AuthHead.jsx";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function LoginForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-    const initialValues = {
-      email: "",
-      password: "",
-    };
+  const [type, setType] = useState("password");
+  const togglePassword = () => {
+    setType(type === "password" ? "text" : "password");
+  };
+  const initialValues = {
+    email: "",
+    password: "",
+  };
   const handleSubmit = async (values, { resetForm }) => {
-      try {
-        await dispatch(login(values)).unwrap();
-        resetForm();
-        navigate("/recommended");        
-      } catch (error) {
-        console.log(error);        
-      }
+    try {
+      await dispatch(login(values)).unwrap();
+      resetForm();
+      navigate("/recommended");
+    } catch (error) {
+      console.log(error);
     }
-    return (
-      <div className={css.login_form}>
-        <AuthHead />
-        <Formik
-          initialValues={initialValues}
-          validationSchema={loginSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form className={css.form}>
-            <Field name="email" type="email" placeholder="Email" />
+  };
+  return (
+    <div className={css.login_form}>
+      <AuthHead />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.form}>
+          <div className={css.fieldWrapper}>
+            <label className={css.floatingLabel}>
+              <span className={css.labelText}>Mail:</span>
+              <Field name="email" type="email" className={css.input} />
+            </label>
+            <ErrorMessage name="email" component="div" className={css.error} />
+          </div>
+
+          <div className={css.fieldWrapper}>
+            <label className={css.floatingLabel}>
+              <span className={css.labelText}>Password:</span>
+              <Field
+                name="password"
+                type={type}
+                className={`${css.input} ${css.leftPadding}`}
+              />
+              <button
+                type="button"
+                onClick={togglePassword}
+                className={css.eyeBtn}
+              >
+                <svg className={css.eyeIco} width="20" height="20">
+                  {type === "password" ? (
+                    <use href="/icons/sprite.svg#icon-eye-off" />
+                  ) : (
+                    <use href="/icons/sprite.svg#icon-eye" />
+                  )}
+                </svg>
+              </button>
+            </label>
+            <ErrorMessage
+              name="password"
+              component="div"
+              className={css.error}
+            />
+          </div>
+          {/* <Field name="email" type="email" placeholder="Email" />
             <ErrorMessage name="email" component="div" className={css.error} />
             <Field name="password" type="password" placeholder="Password" />
             <ErrorMessage
               name="password"
               component="div"
               className={css.error}
-            />
-            <div>
-              <button type="submit" className={css.button}>
-                Log In
-              </button>
-              <Link to="/register" className={css.registerwrapp_link}>
-                Don’t have an account?
-              </Link>
-            </div>
-          </Form>
-        </Formik>
-      </div>
-    );
+            /> */}
+          <div className={css.logInBtns}>
+            <button type="submit" className={css.button}>
+              Log In
+            </button>
+            <Link to="/register" className={css.registerwrapp_link}>
+              Don’t have an account?
+            </Link>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,12 +2,17 @@ import { Formik, Form, ErrorMessage, Field } from "formik";
 import { initialValues, registerSchema } from "../formSchema";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { register } from "../../redux/auth/operations.js";
 import css from "./RegistrationForm.module.css";
 import AuthHead from "../AuthHead/AuthHead.jsx";
 
 export default function RegistrationForm() {
   const dispatch = useDispatch();
+  const [type, setType] = useState("password");
+  const togglePassword = () => {
+    setType(type === "password" ? "text" : "password");
+  };
   const handleSubmit = (values, { resetForm }) => {
     console.log("Form data", values);
     dispatch(register(values)).unwrap();
@@ -22,6 +27,61 @@ export default function RegistrationForm() {
         onSubmit={handleSubmit}
       >
         <Form className={css.form}>
+          {/* ===== Поле Name ===== */}
+          <div className={css.fieldWrapper}>
+            <label className={css.floatingLabel}>
+              <span className={css.labelText}>Name:</span>
+              <Field name="name" type="text" className={css.input} />
+            </label>
+            <ErrorMessage name="name" component="div" className={css.error} />
+          </div>
+
+          {/* ===== Поле Email ===== */}
+          <div className={css.fieldWrapper}>
+            <label className={css.floatingLabel}>
+              <span className={css.labelText}>Mail:</span>
+              <Field name="email" type="email" className={css.input} />
+            </label>
+            <ErrorMessage name="email" component="div" className={css.error} />
+          </div>
+
+          {/* ===== Поле Password з оком ===== */}
+          <div className={css.fieldWrapper}>
+            <label className={css.floatingLabel}>
+              <span className={css.labelText}>Password:</span>
+              <Field name="password" type={type} className={css.input} />
+              <button
+                type="button"
+                onClick={togglePassword}
+                className={css.eyeBtn}
+                aria-label="Toggle password"
+              >
+                <svg width="20" height="20" className={css.eyeIcon}>
+                  <use
+                    href={`/icons/sprite.svg#${
+                      type === "password" ? "icon-eye-off" : "icon-eye"
+                    }`}
+                  />
+                </svg>
+              </button>
+            </label>
+            <ErrorMessage
+              name="password"
+              component="div"
+              className={css.error}
+            />
+          </div>
+
+          <div className={css.btnWrapper}>
+            <button type="submit" className={css.button}>
+              Registration
+            </button>
+            <Link to="/login" className={css.registerwrapp_link}>
+              Already have an account?
+            </Link>
+          </div>
+        </Form>
+        {/* <Form className={css.form}>
           <Field name="name" type="text" placeholder="Username" />
           <ErrorMessage name="name" component="div" className={css.error} />
           <Field name="email" type="email" placeholder="Email" />
@@ -36,7 +96,7 @@ export default function RegistrationForm() {
               Already have an account?
             </Link>
           </div>
-        </Form>
+        </Form> */}
       </Formik>
     </div>
   );
